1691b051430b3977d20d7ac4bf76aa9224db3784
Eugene Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-02-12 07:46:59 +0200

    fix button styles

--- src/components/ToggleDevMode/ModeContext.jsx
+++ src/components/ToggleDevMode/ModeContext.jsx
@@ -2,4 +2,5 @@ import { createContext, useContext } from 'react';
 
 const ModeContext = createContext();
 export default ModeContext;
+// eslint-disable-next-line react-refresh/only-export-components
 export const useModeContext = () => useContext(ModeContext);
